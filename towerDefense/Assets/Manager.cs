95caef97cf009b5ff0c8b536aee54f0c18ef7325
Nick Carroll <ncarroll@nd.edu>
2016-12-07 19:44:00 -0500

    added the unity project files

---
+++ towerDefense/Assets/Manager.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Manager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] tilePrefabs;
+
+//	[SerializeField]
+//	private CameraMovement cameraMovement;
+
+
+	public GameObject Hero;
+
+
+	public float tileSize
+	{
+		get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+	}
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		CreateLevel();
+	}
+
+	private void CreateLevel()
+	{
+		string[] mapData = ReadLevelText();
+
+
+		int mapX = mapData[0].ToCharArray().Length;
+		int mapY = mapData.Length;
+
+
+
+		//char[] newTiles = mapData [0].ToCharArray ();
+
+
+		Vector3 maxTile = Vector3.zero;
+
+		Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 0));
+
+
+		Vector3 heroStart = new Vector3 (worldStart.x, worldStart.y, -1.0f);
+
+
+		Hero.transform.position = heroStart;
+
+		//Debug.Log (newTiles[0]);
+
+
+		for(int y = 0; y < mapY; y++)
+		{
+
+		
+
+			char[] newTiles = mapData[y].ToCharArray();
+
+
+
+
+
+			for(int x = 0; x < mapX; x++)
+			{
+				string entry = "";
+
+				
+				entry += newTiles [x];
+
+
+
+				maxTile = PlaceTile(entry, x, y, worldStart); 
+			}
+				
+		}
+
+		//cameraMovement.SetLimits(new Vector3(maxTile.x + tileSize, maxTile.y - tileSize));
+	}
+
+	private Vector3 PlaceTile(string tileType, int x, int y, Vector3 worldStart)
+	{
+		int tileIndex = int.Parse(tileType); 
+
+		Debug.Log (tileType);
+
+
+
+		GameObject newTile = Instantiate(tilePrefabs[tileIndex]);
+		newTile.transform.position = new Vector3(worldStart.x + (tileSize * x), worldStart.y - (tileSize * y), 0);
+		return newTile.transform.position;
+	}
+
+	private string[] ReadLevelText()
+	{
+
+
+
+		TextAsset bindData = Resources.Load("Level") as TextAsset;
+
+
+		//TextAsset map = (TextAsset)Resources.Load("Level", typeof(TextAsset));
+
+
+
+		Debug.Log ("here");
+
+		//Debug.Log (bindData);
+
+		string data = bindData.text.Replace(Environment.NewLine, string.Empty);
+
+
+
+		Debug.Log (data);
+		Debug.Log ("data");
+
+
+		return data.Split('-');
+	}
+}
---
+++ towerDefense/Assets/movement.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+
+
+
+	public GameObject apple;
+
+
+	public Vector3 applePosition;
+
+
+
+
+	public float tileSize
+	{
+		get { return this.GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+	}
+
+	private float threshold;
+	private float time;
+
+	// Use this for initialization
+	void Start () {
+		time = 0;
+		threshold = 1.0f;
+
+
+
+	applePosition = apple.transform.position;
+
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+
+
+		Vector3 applePositon = apple.transform.position;
+
+
+		//Vector3 moveX = new Vector3 ( 0.1f, 0.0f, 0.0f);
+		//ector3 moveY = new Vector3 ( 0.0f, 0.1f, 0.0f);
+
+		Vector3 moveX = new Vector3( tileSize ,0.0f , 0);
+		Vector3 moveY = new Vector3( 0.0f ,tileSize , 0);
+
+
+
+
+		//newPos= curr + direction * tileSize
+
+
+			//Vector3( tileSize,0.0f, 0);
+
+
+
+		if (time > threshold) {
+			time = 0.0f;
+
+			if (this.transform.position.x < applePositon.x) {
+
+
+				this.transform.Translate (moveX);
+
+
+			} else if (this.transform.position.x > applePositon.x) {
+
+
+				this.transform.Translate (-moveX);
+			} else if (this.transform.position.y < applePositon.y) {
+
+
+				this.transform.Translate (moveY);
+
+
+			} else if (this.transform.position.y > applePositon.y) {
+
+
+				this.transform.Translate (-moveY);
+			}
+	
+		}
+	
+	}
+}
